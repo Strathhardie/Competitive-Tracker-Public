357f50ce8882526badab2684efdd9acb25a09b20
Ashwin97 <ashwin.kamalakannan@cibc.com>
2018-11-06 16:09:56 -0500

    added excel unzip directory and rezipped example

--- zip-util.sh
+++ zip-util.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
+cd ./Excel\ Scanner || { echo "error: Excel Scanner/ does not exist"; exit 1; }
 mkdir -p ./excel_dump || { echo "error: excel_dump/ could not be created"; exit 1; }
 
 if [ $1 == 'unzip' ]
 then
-	unzip -u Excel\ Scanner/competitive_scanner.xlsm -d ./excel_dump || { echo "error: can't unzip to excel_dump/"; exit 1; }
+	unzip -u competitive_scanner.xlsm -d ./excel_dump || { echo "error: can't unzip to excel_dump/"; exit 1; }
 
 elif [ $1 == 'zip' ]
 then
 	cd ./excel_dump
-	zip -r ../Excel\ Scanner/competitive_scanner2.xlsm $(ls) || { echo "error: excel dump is empty, please unzip file first"; exit 1; }
+	zip -r ../competitive_scanner--MODIFIED.xlsm $(ls) || { echo "error: excel dump is empty, please unzip file first"; exit 1; }
 	cd ..
 
 else
