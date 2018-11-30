1fd6607bfed20423f6492903283b7601286d693d
Ashwin97 <Ashwin.KamalaKannan@cibc.com>
2018-11-30 16:42:47 -0500

    finished excel version control script for python

--- Excel Scanner/excel_util.py
+++ Excel Scanner/excel_util.py
@@ -1,6 +1,15 @@
 import win32com.client as win32
 import sys, os, json, zipfile
 
+def getArgs():
+	try:
+		with open(os.path.join(os.path.dirname(__file__), 'excel_util_config.json'), 'r') as file:
+			return json.load(file)
+	except Exception as e:
+		print("Error config file missing.")
+		print(e)
+	return {}
+
 def exportModules(wb, app):
 	try:
 		print("Exporting excel modules...", end=" ")
@@ -28,15 +37,19 @@ def unzip():
 		print("Done.")
 	except Exception as e:
 		print("Error.")
-
-def getArgs():
-	try:
-		with open(os.path.join(os.path.dirname(__file__), 'excel_util_config.json'), 'r') as file:
-			return json.load(file)
-	except Exception as e:
-		print("Error config file missing.")
 		print(e)
-	return {}
+
+def zip():
+	try:
+		print("Zipping excel file...", end=" ")
+		with zipfile.ZipFile(args['modified_workbook_path'], 'w') as zip_ref:
+			for root, dirs, files in os.walk(args['dump_path']):
+				for file in files:
+					zip_ref.write(os.path.join(root, file), os.path.join('.' + root[12:], file), zipfile.ZIP_DEFLATED)
+		print("Done.")
+	except Exception as e:
+		print("Error.")
+		print(e)
 
 ###############################################
 
@@ -53,6 +66,7 @@ try:
 		unzip()
 	elif sys.argv[1] == "zip":
 		importModules(wb, excel.Application)
+		zip()
 	else:
 		print('Error: invalid argument \'{}\''.format(sys.argv[1]))
 
