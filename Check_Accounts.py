5d9cd2e67db5260bae8378a23375ddf8c348362c
polly-yao <Polly.Yao@cibc.com>
2019-08-22 14:09:07 -0400

    Update Check_Accounts.py

--- Check_Accounts.py
+++ Check_Accounts.py
@@ -1,4 +1,4 @@
-from bs4 import BeautifulSoup
+#from bs4 import BeautifulSoup
 from selenium_utils import SeleniumUtils
 
 pathTo = ""
@@ -11,7 +11,7 @@ def findBankElements(bankName,pathTO,bankID,bankTable,NumberOfAccounts):
 
  with open(r'' + pathTO, "r", encoding="utf8") as f:
   page = f.read()
-  soup = BeautifulSoup(page, 'html.parser')
+#  soup = BeautifulSoup(page, 'html.parser')
 
   if bankName=="Simplii" or bankName=="RBC":
      divRowsbank = soup.find('div', id=bankID)
@@ -24,7 +24,7 @@ def findBankElements(bankName,pathTO,bankID,bankTable,NumberOfAccounts):
          print(bankName + " "+ NumberOfAccounts + " " + "Bank account has been deleted")
 
   elif bankName=="CIBC"  or bankName=="Scotiabank" or bankName=="BMO" or bankName=="ICICI" or bankName=="NBC":
-   Bankrows = soup.find_all(bankTable)
+#   Bankrows = soup.find_all(bankTable)
    if len(Bankrows) == len(NumberOfAccounts):
        print("No account has been added in the bank"+ " " + bankName)
    elif len(Bankrows) > len(NumberOfAccounts):
