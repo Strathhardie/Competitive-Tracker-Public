36bb8e469f51c41a322abf3c2005c1989c4548f9
julian-malixi <julian.malixi@cibc.com>
2019-08-27 15:06:07 -0400

    Add feature to check if account is added/removed
    Why:
    
    CT-25
    
    This change addresses the need by:
    
    We utilize the BeautifulSoup Python library to read and parse a given
    bank's webpage for HTML elements. We then extract a subset of the HTML
    and count the number of elements related to the bank accounts. If the
    count matches what we expect to see on the page, then no accounts have
    been added/removed. If it doesn't match, we indicate as such and edit
    the config file to reflect the difference.

--- main.py
+++ main.py
@@ -3,35 +3,33 @@ from selenium_utils import SeleniumUtils
 from parser_utils import ParserUtils
 import datetime
 import os
-from Check_Accounts import saveDataFiles
-from Check_Accounts import findBankElements
+import requests
+from bs4 import BeautifulSoup
 
 def main():
-
     banks = YAMLUtils.readYAML(YAMLUtils.FILE_NAME)
     # 1. Save the current HTML with format <financial institution>-<account type>-<current date time>.html
     dt = '{:%Y%m%d_%H%M%S}'.format(datetime.datetime.now())
     # Iterate through each bank in the YAML
 
     for bank in banks:
+      if bank['name'] == 'Simplii' or bank['name'] == 'CIBC':
         # Make a dictionary where key:value is filepath:xpath
         bankData = dict()
         # Go through each account in the bank
         for account in bank['accounts']:
           # Save each account's filepath, and the xpath to the dictionary
-
             if account['xpath'] == "none":
                 print("")
             else:
-             bankData['websites/' + bank['name'] + '-' + account['account_name'] + '-' + dt + ".html"] = account['xpath'];
-
-
+                bankData['websites/' + bank['name'] + '-' + account['account_name'] + '-' + dt + ".html"] = account['xpath']
+                
         # Save the obtained HTML to the filepath
-       # 2. Compare the downloaded html to the most recent previous html with the same account type and financial institution
+        # 2. Compare the downloaded html to the most recent previous html with the same account type and financial institution
         try:
             SeleniumUtils.saveBankAccountsXPathHTML(bank['url'], bankData)
         except(Exception) as error:
-            print(error);
+            print(error)
         for filepath in bankData:
             # Read the downloaded file contents, bank name, account, and datetime
             currSrcFile = open(filepath, encoding='utf-8')
@@ -53,15 +51,30 @@ def main():
                     prevSrc = prevSrcFile.read()
                     break
             # 3. Output whether the current xpath text has changed from the previous xpath text
-
             if (prevSrc == currSrc):
                 print("No change in " + bank['name'] + "-" + currSrcAccount)
             else:
                 print("Change detected in " + bank['name'] + "-" + currSrcAccount)
 
-        saveDataFiles(bank['url'],'WebsitesHTML/' + bank['name']  + '-' + dt + ".html")
-        findBankElements(bank['name'],'WebsitesHTML/' + bank['name']  + '-' + dt + ".html",bank['id'],bank['table'],bank['accounts'])
-
+        # 4. Count the number of accounts for a given banks to determine if one is added or removed
+        # EQ bank is currently out of scope due to having only one bank account, not in a table
+        if bank['name'] != 'EQ_Bank':
+            path = 'full_websites/' + bank['name'] + '.html'
+            SeleniumUtils.saveSourceHTML(bank['url'], path)
+            with open(path,encoding="utf-8") as f:
+                global count; count = 0
+                data = f.read()     
+                global soup; soup = BeautifulSoup(data, 'html.parser')    
+                # Based on the functions in the yaml, search the HTML page for specific elements and count them       
+                for fn in bank['functions']:
+                    command = 'count += str(' + fn['function'] + '(' + fn['param'] + ')' + fn['suffix'] + ').count(\'' + fn['count'] + '\')'
+                    exec(command, globals())
+                # If the count is not equal to the original number of accounts, output message and update the yaml
+                if count != bank['total_count']:
+                    print(bank['name'], ": Account added or removed!")
+                    YAMLUtils.writeYAML(YAMLUtils.FILE_NAME, bank['name'], count)
+                else:
+                    print(bank['name'], ": All good")
 
 main()
 
