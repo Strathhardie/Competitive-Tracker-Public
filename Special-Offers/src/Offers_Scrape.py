007cad6497da49e4707b1a4f797a7745f5131234
hunghanchen <chenhung@sheridancollege.ca>
2020-12-16 23:36:34 -0500

    final

--- Special-Offers/src/Offers_Scrape.py
+++ Special-Offers/src/Offers_Scrape.py
@@ -7,6 +7,7 @@ import glob
 import scraper
 import json
 import compare_Excel
+import string
 from openpyxl import Workbook
 from openpyxl.styles import Alignment
 from openpyxl.styles import Font
@@ -22,76 +23,137 @@ from pathlib import Path
 headers =['Bank','Account Name','Account Type', 'Monthly Fee', 'Special Offer','Expiry Date', 'Account Perks', 'Webiste']
 book = Workbook()
 sheet = book.active
+sheet.title="Saving"
+sheet2 = book.create_sheet("Chequing")
 
 for index, header in enumerate(headers):
     sheet.cell(row=1, column=index+1).font = Font(bold=True)
-    sheet.cell(row=1, column=index+1).value=header
+    sheet.cell(row=1, column=index+1).value=header    
+    sheet2.cell(row=1, column=index+1).font = Font(bold=True)
+    sheet2.cell(row=1, column=index+1).value=header
 
 # 1. Get dictionary return value from scraper.py for sepecial offer.
 # print(json.dumps(scraper.get_special_offer_accounts(), indent=1))
 special_offer_dict = scraper.get_special_offer_accounts()
-# print(dic)
+
 
 def getList(dict): 
     return special_offer_dict.keys() 
-# print(getList(dict))
 
-# print("dict "+ len(dict.keys()))
-#Get bank name
-# print(dict[0]['institution_name'])
+rowNumSav = 2
+rowNumChe = 2
 
-rowNum = 2
+def writeIntoWorkBook(SavOrCheSheet, bankName, rowIndex): 
+    # SavOrCheSheet = sheet
+    SavOrCheSheet.cell(row=rowIndex, column=1).alignment = Alignment(horizontal='center', vertical='center')
+    SavOrCheSheet.cell(row=rowIndex, column=1).value=bankName
+
+    SavOrCheSheet.cell(row=rowIndex, column=2).alignment = Alignment(horizontal='center', vertical='center')
+    SavOrCheSheet.cell(row=rowIndex, column=2).value=special_offer_dict[x]['accounts'][y]['account_name'][0]  
+
+    accountName = special_offer_dict[x]['accounts'][y]['account_name'][0]
+
+    SavOrCheSheet.cell(row=rowIndex, column=3).alignment = Alignment(horizontal='center', vertical='center')
+    SavOrCheSheet.cell(row=rowIndex, column=3).value=special_offer_dict[x]['accounts'][y]['account_category']
+
+    SavOrCheSheet.cell(row=rowIndex, column=4).alignment= Alignment(wrapText=True)
+    # SavOrCheSheet.cell(row=rowNum, column=4).alignment = Alignment(horizontal='left', vertical='center')
+    writtenPara=""
+    if not special_offer_dict[x]['accounts'][y]['fee']:
+        writtenPara ="$0"
+    else:            
+        for index, fee in enumerate(special_offer_dict[x]['accounts'][y]['fee']):           
+            writtenPara += str(index+1)+". "+fee+"\n"   
+    SavOrCheSheet.cell(row=rowIndex, column=4).value=writtenPara  
+
+    SavOrCheSheet.cell(row=rowIndex, column=5).alignment= Alignment(wrapText=True)
+    writtenPara=""
+    if not special_offer_dict[x]['accounts'][y]['special_offer']:
+        writtenPara ="No Data!"
+    else:            
+        for index, special_offer in enumerate(special_offer_dict[x]['accounts'][y]['special_offer']):           
+            # writtenPara += str(index+1)+". "+special_offer+"\n"
+            if accountName == 'The MomentumPLUS Savings Account':
+                if index == 6:
+                    break
+
+            if accountName == 'RBC High Interest eSavings':
+                s1 = str(special_offer)
+                s2 = s1.replace('1:', ':')
+                special_offer = s2
+                if index == 1:
+                    break
+            if accountName == 'Savings Builder Account':
+                if index == 2:
+                    break
+                s3 = str(special_offer)
+                s4 = s3.replace("*16", ".")
+                s5 = s4.replace("*17", "")
+                s6 = s5.replace(". when", " when")
+                special_offer = s6
+
+            det1 = str(special_offer)
+            cleaned = det1.replace('legal bug', '').rstrip(string.digits)
+            # print("$$$"+cleaned)
+            writtenPara += str(index + 1) + ". " + cleaned + "\n"
+
+    SavOrCheSheet.cell(row=rowIndex, column=5).value=writtenPara     
+    # SavOrCheSheet.cell(row=rowNum, column=4).value=special_offer_dict[x]['accounts'][y]['fee']
+
+    writtenPara=""
+
+    for index, detail in enumerate(special_offer_dict[x]['accounts'][y]['details']):
+        # writtenPara += str(index+1)+". "+detail+"\n"
+        det = str(detail)
+        cleaned = det.replace('legal bug', '').rstrip(string.digits)
+        writtenPara += str(index + 1) + ". " + cleaned + "\n"
+    SavOrCheSheet.cell(row=rowIndex, column=7).value = writtenPara
+
+    SavOrCheSheet.cell(row=rowIndex, column=8).alignment = Alignment(horizontal='center', vertical='center')
+    SavOrCheSheet.cell(row=rowIndex, column=8).value=special_offer_dict[x]['accounts'][y]['account_url']
+
+
+    rowIndex +=1
+
+
+
+
+
 for x in getList(special_offer_dict):
     #Get Bank name
     bankName=special_offer_dict[x]['institution_name']
 
     for y in range(len(special_offer_dict[x]['accounts'])):
-        sheet.cell(row=rowNum, column=1).alignment = Alignment(horizontal='center', vertical='center')
-        sheet.cell(row=rowNum, column=1).value=bankName
+        #We need to seperate Saving account and Chequing account
+        if special_offer_dict[x]['accounts'][y]['account_category']=="Saving Accounts":
 
-        sheet.cell(row=rowNum, column=2).alignment = Alignment(horizontal='center', vertical='center')
-        sheet.cell(row=rowNum, column=2).value=special_offer_dict[x]['accounts'][y]['account_name'][0]  
+            writeIntoWorkBook(sheet, bankName, rowNumSav)
+            rowNumSav +=1
 
-        sheet.cell(row=rowNum, column=3).alignment = Alignment(horizontal='center', vertical='center')
-        sheet.cell(row=rowNum, column=3).value=special_offer_dict[x]['accounts'][y]['account_category']
+           
+        else:
 
-        sheet.cell(row=rowNum, column=4).alignment= Alignment(wrapText=True)
-        # sheet.cell(row=rowNum, column=4).alignment = Alignment(horizontal='left', vertical='center')
-        writtenPara=""
-        if not special_offer_dict[x]['accounts'][y]['fee']:
-            writtenPara ="No Data!"
-        else:            
-            for index, fee in enumerate(special_offer_dict[x]['accounts'][y]['fee']):           
-                writtenPara += str(index+1)+". "+fee+"\n"   
-        sheet.cell(row=rowNum, column=4).value=writtenPara  
 
-        sheet.cell(row=rowNum, column=5).alignment= Alignment(wrapText=True)
-        writtenPara=""
-        if not special_offer_dict[x]['accounts'][y]['special_offer']:
-            writtenPara ="No Data!"
-        else:            
-            for index, special_offer in enumerate(special_offer_dict[x]['accounts'][y]['special_offer']):           
-                writtenPara += str(index+1)+". "+special_offer+"\n"
-        sheet.cell(row=rowNum, column=5).value=writtenPara     
-        # sheet.cell(row=rowNum, column=4).value=special_offer_dict[x]['accounts'][y]['fee']
+            writeIntoWorkBook(sheet2, bankName, rowNumChe)
+            rowNumChe += 1
 
-        writtenPara=""
-        for index, detail in enumerate(special_offer_dict[x]['accounts'][y]['details']):
-            writtenPara += str(index+1)+". "+detail+"\n"
-        sheet.cell(row=rowNum, column=7).value=writtenPara
 
-        sheet.cell(row=rowNum, column=8).alignment = Alignment(horizontal='center', vertical='center')
-        sheet.cell(row=rowNum, column=8).value=r'=HYPERLINK("'+special_offer_dict[x]['accounts'][y]['account_url']+'","'+bankName+'")'
-        # print(special_offer_dict[x]['accounts'][y]['account_url'])
 
-        rowNum +=1
+            
 
 
 # #Today date in order to generate Excel timestamp
 todayDate = str(date.today())
 
+
+
+
+
 #Name convention for excel file "specialOfferYYYY-MM-DD"
 book.save("specialOffer_"+todayDate+".xlsx")
+
+
+
 
 #show different in excel compare last execute
 compare_Excel.compare_changed_Special_Offer()
--- Special-Offers/src/compare_Excel.py
+++ Special-Offers/src/compare_Excel.py
@@ -5,33 +5,7 @@ import pandas as pd
 from pathlib import Path
 from datetime import date
 
-
-    
-
-
-def compare_changed_Special_Offer():
-
-
-
-    #Today date in order to generate Excel timestamp
-    todayDate = str(date.today())
-
-    #Get correct file
-    excelFile=glob.glob("specialOffer_[0-9]*.xlsx")
-    excelFile = sorted(excelFile)
-
-    #path to files
-    currentDirectory = os.getcwd()+"\\"
-    path_OLD=Path(currentDirectory+excelFile[len(excelFile)-2])
-    path_NEW=Path(currentDirectory+excelFile[len(excelFile)-1])
-
-
-
-    # Read in the two excel files and fill NA
-    df_OLD = pd.read_excel(path_OLD,header=None, names=None).fillna(0)
-    df_NEW = pd.read_excel(path_NEW,header=None, names=None).fillna(0)
-
-
+def write_Sav_Che_Sheet(df_OLD,df_NEW):
     dfDiff = df_OLD.copy()
     for row in range(dfDiff.shape[0]):
         for col in range(dfDiff.shape[1]):
@@ -40,24 +14,28 @@ def compare_changed_Special_Offer():
                 value_NEW = df_NEW.iloc[row,col]
                 if value_OLD==value_NEW and value_NEW!=0:
                     dfDiff.iloc[row,col] = df_NEW.iloc[row,col]
-                    print(df_NEW.iloc[row,col])
                 elif value_OLD==value_NEW and value_NEW==0:
                     dfDiff.iloc[row,col] = ""
                 elif( value_OLD!=value_NEW and value_NEW==0):
-                    dfDiff.iloc[row,col] = ('Expired Offers: {}').format(value_OLD)
+                    dfDiff.iloc[row,col] = ('Expired Offers:\n{}').format(value_OLD)
                 else:
-                    dfDiff.iloc[row,col] = ('Update To: {}').format(value_NEW)
-
+                    dfDiff.iloc[row,col] = ('Update To:\n{}').format(value_NEW)
+                   
             except:
                 dfDiff.iloc[row,col] = ('{}-->{}').format(value_OLD, 'NaN')
-    writer = pd.ExcelWriter("specialOffer_compare"+todayDate+".xlsx", engine='xlsxwriter') # pylint: disable=abstract-class-instantiated
-    dfDiff.to_excel(writer, sheet_name='DIFF', index=False, header=None)
 
+    for row in range(dfDiff.shape[0]):
+        for col in range(dfDiff.shape[1]):
+            if row >0 and col ==7:
+                if len(dfDiff.iloc[row, col]) <= 255:
+                    dfDiff.iloc[row, col] = '=HYPERLINK("{0}","{1}")'.format(dfDiff.iloc[row, col],dfDiff.iloc[row, 0])
 
-
-
+    print(dfDiff)
+    return dfDiff
+    
+def write_style_sheet(writer, SavOrCheSheet):
     workbook  = writer.book
-    worksheet = writer.sheets['DIFF']
+    worksheet = writer.sheets[SavOrCheSheet]
 
 
     # define formats
@@ -78,25 +56,134 @@ def compare_changed_Special_Offer():
     # Style Excel Format
 
     # Add a header format.
+    header_format_blank = workbook.add_format({
+        'bold': True,
+        'text_wrap': True,
+        'valign': 'vcenter',
+        'align': 'center', 
+        'fg_color': '#FFFFFF',
+        'border': 1})
+    worksheet.set_column("I:Z", None, header_format_blank)
+
+    # Add a header format.
     header_format = workbook.add_format({
         'bold': True,
         'text_wrap': True,
-        'valign': 'top',
+        'valign': 'vcenter',
+        'align': 'center', 
         'fg_color': '#D7E4BC',
         'border': 1})
     worksheet.set_row(0, None, header_format)
 
     # Add a first row "Bank" format.
     first_column_format = workbook.add_format({
-        'bold': True,
+        'bold': False,
         'text_wrap': True,
-        # 'valign': 'center',
         'align': 'center', 
         'valign': 'vcenter',
-        # 'fg_color': '#D7E4BC',
         'border': 1})
-    worksheet.set_column('A:A', 20, first_column_format)
+    worksheet.set_column('A:A', 15, first_column_format)
 
+    # Add a second row "Account Name" format.
+    second_column_format = workbook.add_format({
+        'bold': False,
+        'text_wrap': True,
+        'align': 'center', 
+        'valign': 'vcenter',
+        'border': 1})
+    worksheet.set_column('B:B', 50, second_column_format)
+
+    # Add a third row "Account Type" format.
+    third_column_format = workbook.add_format({
+        'bold': False,
+        'text_wrap': True,
+        'align': 'center', 
+        'valign': 'vcenter',
+        'border': 1})
+    worksheet.set_column('C:C', 25, third_column_format)
+
+    # Add a forth row "Monthly Fee" format.
+    forth_column_format = workbook.add_format({
+        'bold': False,
+        'text_wrap': True,
+        'align': 'left', 
+        'valign': 'vcenter',
+        'border': 1})
+    worksheet.set_column('D:D', 50, forth_column_format)
+
+    # Add a fifth row "Special Offer" format.
+    fifth_column_format = workbook.add_format({
+        'bold': False,
+        'text_wrap': True,
+        'align': 'left', 
+        'valign': 'vcenter',
+        'border': 1})
+    worksheet.set_column('E:E', 50, fifth_column_format)
+
+    # Add a sixth row "Expiry Date" format.
+    sixth_column_format = workbook.add_format({
+        'bold': False,
+        'text_wrap': True,
+        'align': 'left', 
+        'valign': 'vcenter',
+        'border': 1})
+    worksheet.set_column('F:F', 15, sixth_column_format)
+
+    # Add a seventh row "Account Perks" format.
+    seventh_column_format = workbook.add_format({
+        'bold': False,
+        'text_wrap': True,
+        'align': 'left', 
+        'valign': 'vcenter',
+        'border': 1})
+    worksheet.set_column('G:G', 50, seventh_column_format)    
+
+    # Add a eighth row "Webiste" format.
+    ninth_column_format = workbook.add_format({
+        'bold': False,
+        'text_wrap': True,
+        'align': 'center', 
+        'valign': 'vcenter',
+        'border': 1})
+    worksheet.set_column('H:H', 30, ninth_column_format)   
+
+    # Freeze the first row.
+    worksheet.freeze_panes(1, 0) 
+
+
+
+def compare_changed_Special_Offer():
+    
+
+    #Today date in order to generate Excel timestamp
+    todayDate = str(date.today())
+
+    #Get correct file
+    excelFile=glob.glob("specialOffer_[0-9]*.xlsx")
+    excelFile = sorted(excelFile)
+
+    #path to files
+    currentDirectory = os.getcwd()+"\\"
+    path_OLD=Path(currentDirectory+excelFile[len(excelFile)-2])
+    path_NEW=Path(currentDirectory+excelFile[len(excelFile)-1])
+
+    # Read in the two excel files and fill NA
+    df_OLD_Sav = pd.read_excel(path_OLD,sheet_name="Saving", header=None, names=None).fillna(0)
+    df_NEW_Sav = pd.read_excel(path_NEW,sheet_name="Saving",header=None, names=None).fillna(0)
+    df_OLD_Che = pd.read_excel(path_OLD,sheet_name="Chequing",header=None, names=None).fillna(0)
+    df_NEW_Che = pd.read_excel(path_NEW,sheet_name="Chequing",header=None, names=None).fillna(0)
+
+
+    writer = pd.ExcelWriter("specialOffer_compare"+todayDate+".xlsx", engine='xlsxwriter') # pylint: disable=abstract-class-instantiated
+
+    write_Sav_Che_Sheet(df_OLD_Sav,df_NEW_Sav).to_excel(writer, sheet_name='DIFF_Sav', index=False, header=None)
+    write_Sav_Che_Sheet(df_OLD_Che,df_NEW_Che).to_excel(writer, sheet_name='DIFF_Che', index=False, header=None)
+
+
+   
+
+    write_style_sheet(writer, "DIFF_Sav")
+    write_style_sheet(writer, "DIFF_Che")
 
     # save
     writer.save()
